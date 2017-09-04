d5f654a62ad3e8cc9213735d1603140c18759810
JohnPlayerSpecial <cs.prio30@gmail.com>
2017-09-04 23:57:18 +0200

    Add files via upload

---
+++ dw_slow_spoken_news.py
@@ -0,0 +1,93 @@
+from __future__ import print_function
+import feedparser
+from textblob import TextBlob
+import textblob
+from textblob import *
+from telegraphapi import Telegraph
+import telegram
+from telegram.error import Unauthorized, NetworkError
+import re
+from newspaper import Article
+from random import randint
+import sys
+import requests
+from telegram.ext import *
+import datetime
+import time
+
+
+#TOKEN_TELEGRAM = os.environ['TOKEN_TELEGRAM']
+#TELEGRAPH_ACCOUNT = os.environ['TELEGRAPH_ACCOUNT']
+
+TOKEN_TELEGRAM = '377926488:AAHr81UaMC-gBKHDXd-UbpnL0QkD8T_2nlo'
+bot = telegram.Bot(TOKEN_TELEGRAM)
+TELEGRAPH_ACCOUNT = 'f126ck'
+telegraph = Telegraph()
+telegraph.createAccount(TELEGRAPH_ACCOUNT)
+
+def start(bot, update):
+    text = "Welcome!\nYou will receive daily news starting from tomorrow."
+    update.message.reply_text(text)
+
+def getArticleImage(url):
+	article = Article(url)
+	article.download()
+	article.parse()
+	text = article.text
+	articleImage = article.top_image
+	return articleImage
+
+def getTelegraphText(rawString,url):
+	string = ""
+	string += '<img src="{}"></img>\n'.format( getArticleImage(url) )
+	rawString = re.sub( r"^Trainiere dein Hörverstehen mit den Nachrichten der Deutschen Welle von [a-zA-z]+ – als Text und als verständlich gesprochene Audio-Datei." , "" , rawString) # elimina
+	rawString = rawString.replace("\n\n","\n")
+	blob = TextBlob(rawString) 
+	for sentence in blob.sentences:
+		string = string + '<b>{}</b>\n<i>{}</i>\n\n'.format( sentence, str( sentence.translate(to="en") ) )
+	return string
+
+def getCaptionText(title, mp3Size):
+	string = ""
+	string += title.split("–")[0].strip()
+	string += "\n" + str(  round( int(mp3Size) / (1024*1024), 2 )  ) + " MB." 
+	return string
+	
+def getDailyNews():
+	url = 'http://partner.dw.com/xml/DKpodcast_lgn_de'
+	entries = feedparser.parse( url ).entries
+	i = 0
+	title = entries[i].title
+	summary = entries[i].summary
+	link = entries[i].link
+	publishedDate = entries[i].published
+	mp3Url = entries[i].links[1]['href']
+	mp3Size = entries[i].links[1]['length']
+	page = telegraph.createPage( title = title,  html_content= getTelegraphText(summary, link), author_name="DW - Langsam gesprochene Nachrichten" )
+	url2send = 'http://telegra.ph/' + page['path']
+	text = '<a href="{}">[DW]</a>\n<b>{}</b>\n\nOriginal <a href="{}">Post</a> published on {}'.format(url2send, title, link, publishedDate) + "\n" + u"\u2063" #it is the invisible character: stealth mode here motherfucker!!!
+	caption = getCaptionText(title, mp3Size)
+	return text, caption, mp3Url
+	
+chat_idList = [31923577]
+
+updater = Updater(TOKEN_TELEGRAM) 
+dp = updater.dispatcher
+updater.dispatcher.add_handler(CommandHandler('start', start))
+
+def ciao(bot, job):
+	text, caption, mp3Url = getDailyNews()
+	for chat_id in chat_idList:
+		bot.sendMessage(chat_id = chat_id, text = text, parse_mode="Html")
+		bot.sendAudio( chat_id = chat_id, audio = mp3Url, caption = caption )
+
+j = updater.job_queue
+timeIWannaGetMsg = 9
+utc_offset_heroku = time.localtime().tm_gmtoff / 3600
+hour = timeIWannaGetMsg + ( int(utc_offset_heroku) - 2 ) # 2 is my offset
+time2 = datetime.time(hour ,0)
+
+j.run_daily(ciao, time2 )
+
+updater.start_polling()
+updater.idle()
